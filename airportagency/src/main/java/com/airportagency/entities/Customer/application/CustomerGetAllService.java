ff4225117ffe3bc9d4ba10d38899a5e6ca75ac7a
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-07-15 16:32:11 -0500

    a

--- airportagency/src/main/java/com/airportagency/entities/Customer/application/CustomerGetAllService.java
+++ airportagency/src/main/java/com/airportagency/entities/Customer/application/CustomerGetAllService.java
@@ -1,5 +1,18 @@
 package com.airportagency.entities.Customer.application;
 
-public class CustomerGetAllService {
+import java.util.List;
 
+import com.airportagency.entities.Customer.domain.entity.Customer;
+import com.airportagency.entities.Customer.domain.service.CustomerRepository;
+
+public class CustomerGetAllService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerGetAllService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    
+    public List<Customer> getAllCustomers(){
+        return customerRepository.findAll();
+    }
 }
--- airportagency/src/main/java/com/airportagency/entities/Customer/infrastucture/in/CustomerConsoleAdapter.java
+++ airportagency/src/main/java/com/airportagency/entities/Customer/infrastucture/in/CustomerConsoleAdapter.java
@@ -3,10 +3,29 @@ package com.airportagency.entities.Customer.infrastucture.in;
 import java.util.Scanner;
 
 import com.airportagency.entities.Customer.application.CustomerCreateService;
+import com.airportagency.entities.Customer.application.CustomerDeleteService;
+import com.airportagency.entities.Customer.application.CustomerGetAllService;
+import com.airportagency.entities.Customer.application.CustomerSearchService;
+import com.airportagency.entities.Customer.application.CustomerUpdateService;
 
 public class CustomerConsoleAdapter {
     Scanner sc = new Scanner(System.in);
 
     private final CustomerCreateService customerCreateService;
     private final CustomerUpdateService customerUpdateService;
+    private final CustomerDeleteService customerDeleteService;
+    private final CustomerSearchService customerSearchService;
+    private final CustomerGetAllService customerGetAllService;
+    
+    public CustomerConsoleAdapter(CustomerCreateService customerCreateService,
+            CustomerUpdateService customerUpdateService, CustomerDeleteService customerDeleteService,
+            CustomerSearchService customerSearchService, CustomerGetAllService customerGetAllService) {
+        this.customerCreateService = customerCreateService;
+        this.customerUpdateService = customerUpdateService;
+        this.customerDeleteService = customerDeleteService;
+        this.customerSearchService = customerSearchService;
+        this.customerGetAllService = customerGetAllService;
+    }
+
+    
 }
