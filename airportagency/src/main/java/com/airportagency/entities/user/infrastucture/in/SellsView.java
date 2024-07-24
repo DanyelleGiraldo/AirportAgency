88720fe93d2e2ceb5e097035ffad53fd54ae87c2
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-07-24 12:54:40 -0500

    modified:   airportagency/src/main/java/com/airportagency/entities/user/infrastucture/in/SellsView.java 	modified:   airportagency/target/classes/com/airportagency/entities/user/infrastucture/in/SellsView.class

--- airportagency/src/main/java/com/airportagency/entities/user/infrastucture/in/SellsView.java
+++ airportagency/src/main/java/com/airportagency/entities/user/infrastucture/in/SellsView.java
@@ -2,7 +2,35 @@ package com.airportagency.entities.user.infrastucture.in;
 
 import java.util.Scanner;
 
+import com.airportagency.entities.BookingStatus.infrastructure.out.BookingStatusrepositorySQL;
+import com.airportagency.entities.Customer.application.CustomerService;
+import com.airportagency.entities.Customer.infrastucture.in.CustomerConsoleAdapter;
+import com.airportagency.entities.Customer.infrastucture.out.CustomerRepositorySQL;
+import com.airportagency.entities.FlightConnection.application.FlightConnectionService;
+import com.airportagency.entities.FlightConnection.infrastructure.in.FlightConnectionConsoleAdapter;
+import com.airportagency.entities.FlightConnection.infrastructure.out.FlightConnectionMySQLRepository;
+import com.airportagency.entities.FlightFares.infrastructure.out.FlightFareSQLRepository;
+import com.airportagency.entities.Plane.infrastructure.out.PlaneSQLRepository;
+import com.airportagency.entities.Trip.application.TripService;
+import com.airportagency.entities.Trip.infrastructure.in.TripConsoleAdapter;
+import com.airportagency.entities.Trip.infrastructure.out.TripSQLRepository;
+import com.airportagency.entities.TripBooking.application.TripBookingService;
+import com.airportagency.entities.TripBooking.infrastructure.in.TripBookingConsoleAdapter;
+import com.airportagency.entities.TripBooking.infrastructure.out.TripBookingSQLRepository;
+import com.airportagency.entities.airport.infrastucture.out.AirportRepositorySQL;
+import com.airportagency.entities.tripbookindetails.infrastructure.out.TripBookingDetailsSQLRepository;
+
 public class SellsView {
+    CustomerRepositorySQL customerRepositorySQL = new CustomerRepositorySQL();
+    TripSQLRepository tripSQLRepository = new TripSQLRepository();
+    TripBookingSQLRepository tripBookingSQLRepository = new TripBookingSQLRepository();
+    BookingStatusrepositorySQL bookingStatusrepositorySQL  = new BookingStatusrepositorySQL();
+    FlightFareSQLRepository flightFareSQLRepository = new FlightFareSQLRepository();
+    TripBookingDetailsSQLRepository tripBookingDetailsSQLRepository = new TripBookingDetailsSQLRepository();
+    FlightConnectionMySQLRepository flightConnectionMySQLRepository = new FlightConnectionMySQLRepository();
+    PlaneSQLRepository planeSQLRepository = new PlaneSQLRepository();
+    AirportRepositorySQL airportRepositorySQL = new AirportRepositorySQL();
+
     public void start(){
         try(Scanner scanner = new Scanner(System.in)){
             boolean salir = false;
@@ -25,16 +53,51 @@ public class SellsView {
 
                 switch (option) {
                     case 1 -> {
-
+                        CustomerService customerService =  new CustomerService(customerRepositorySQL);
+                        TripBookingService tripBookingService = new TripBookingService(tripSQLRepository, customerRepositorySQL, tripBookingSQLRepository, bookingStatusrepositorySQL, flightFareSQLRepository, tripBookingDetailsSQLRepository, flightConnectionMySQLRepository, planeSQLRepository);
+                        TripBookingConsoleAdapter tripBookingConsoleAdapter = new TripBookingConsoleAdapter(tripBookingService, customerService);
+                        tripBookingConsoleAdapter.createTripBooking();
                     }
-                    case 2 -> System.out.println("2");
-                    case 3 -> System.out.println("3");
-                    case 4 -> System.out.println("4");
-                    case 5 -> System.out.println("5");
-                    case 6 -> System.out.println("6");
-                    case 7 -> System.out.println("7");
-                    case 8 -> System.out.println("8");
-                    case 9 -> System.out.println("9");
+                    case 2 -> {
+                        CustomerService customerService =  new CustomerService(customerRepositorySQL);
+                        CustomerConsoleAdapter customerConsoleAdapter = new CustomerConsoleAdapter(customerService);
+                        customerConsoleAdapter.searchCustomer();
+                    }
+                    case 3 -> {
+                        CustomerService customerService =  new CustomerService(customerRepositorySQL);
+                        TripBookingService tripBookingService = new TripBookingService(tripSQLRepository, customerRepositorySQL, tripBookingSQLRepository, bookingStatusrepositorySQL, flightFareSQLRepository, tripBookingDetailsSQLRepository, flightConnectionMySQLRepository, planeSQLRepository);
+                        TripBookingConsoleAdapter tripBookingConsoleAdapter = new TripBookingConsoleAdapter(tripBookingService, customerService);
+                        tripBookingConsoleAdapter.searchTripBooking();
+                    }
+                    case 4 -> {
+                        CustomerService customerService =  new CustomerService(customerRepositorySQL);
+                        CustomerConsoleAdapter customerConsoleAdapter = new CustomerConsoleAdapter(customerService);
+                        customerConsoleAdapter.createCustomer();
+                    }
+                    case 5 -> {
+                        CustomerService customerService =  new CustomerService(customerRepositorySQL);
+                        CustomerConsoleAdapter customerConsoleAdapter = new CustomerConsoleAdapter(customerService);
+                        customerConsoleAdapter.updateCustomer();
+                    }
+                    case 6 -> {
+                        CustomerService customerService =  new CustomerService(customerRepositorySQL);
+                        TripBookingService tripBookingService = new TripBookingService(tripSQLRepository, customerRepositorySQL, tripBookingSQLRepository, bookingStatusrepositorySQL, flightFareSQLRepository, tripBookingDetailsSQLRepository, flightConnectionMySQLRepository, planeSQLRepository);
+                        TripBookingConsoleAdapter tripBookingConsoleAdapter = new TripBookingConsoleAdapter(tripBookingService, customerService);
+                        tripBookingConsoleAdapter.deleteTripBooking();
+                    }
+                    case 7 -> {
+                        TripService tripService = new TripService(tripSQLRepository, bookingStatusrepositorySQL, tripBookingSQLRepository, airportRepositorySQL, customerRepositorySQL);
+                        TripConsoleAdapter tripConsoleAdapter = new TripConsoleAdapter(tripService);
+                        tripConsoleAdapter.searchTrip();
+                    }
+                    case 8 -> {
+                        System.out.println("...");
+                    }
+                    case 9 -> {
+                        FlightConnectionService flightConnectionService = new FlightConnectionService(flightConnectionMySQLRepository);
+                        FlightConnectionConsoleAdapter flightConnectionConsoleAdapter = new FlightConnectionConsoleAdapter(flightConnectionService);
+                        flightConnectionConsoleAdapter.getFlightConnectionByTrip();
+                    }
                     case 10 -> salir = true;
                     default -> System.out.println("Opción no válida, por favor elige una opción entre 1 y 10.");
                 }
